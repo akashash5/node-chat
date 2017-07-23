cbaae604c44e2de423debf40955a803e40a0c972
Akash <akash.ash5@gmail.com>
2017-07-23 20:50:28 +0530

    eventss

--- public/index.js
+++ public/index.js
@@ -3,8 +3,14 @@
 
     socket.on('connect',function (){
       console.log('connect to server');
+
+      socket.emit('createMessage',{ from:"hsaka",text:"hello"});
     });
 
     socket.on('disconnect',function (){
       console.log('disconnected from server');
     });
+socket.on('newMessage',function(message){
+    console.log('new message');
+    console.log(message);
+});
