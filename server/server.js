3e5dd8567aaceddb77de7e19af875102e0a77130
Akash <akash.ash5@gmail.com>
2017-07-24 20:20:35 +0530

    Third

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ io.on('connection',(socket)=>{
   socket.on('disconnect',()=>{
       console.log('disconnected from server');
     });
+    socket.emit('newMessage',{
+      from:'Admin',
+      text:'Welcome to the chat app',
+      createdAt:new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+      from:'Admin',
+      text:'New user joined',
+      createdAt:new Date().getTime()
+    });
     socket.on('createMessage',(message)=>{
       console.log('Create message ',message);
       io.emit('newMessage',{
@@ -24,6 +34,12 @@ io.on('connection',(socket)=>{
         text:message.text,
         createdAt:new Date().getTime()
       });
+      
+      // socket.broadcast.emit('newMessage',{
+      //    from:message.from,
+      //    text:message.text,
+      //    createdAt:new Date().getTime()
+      //  });
     });
 });
 
