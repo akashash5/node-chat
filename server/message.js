b606764dfce86a97d84ad095dd8d6214a064d398
Akash <akash.ash5@gmail.com>
2017-07-24 21:45:23 +0530

    =geolocation

--- server/message.js
+++ server/message.js
@@ -5,4 +5,12 @@ return{
     createdAt:new Date().getTime()
 }
 };
-module.exports={generateMessage};
+var generateLocationMessage=(from,lat,long)=>{
+return{
+    from,
+    url:`https://www.google.com/maps?q=${lat},${long}`,
+    createdAt:new Date().getTime()
+}
+};
+
+module.exports={generateMessage,generateLocationMessage};
