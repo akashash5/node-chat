78c07a6907ffc5469716f3e45c0eee2ad59372ec
Akash <akash.ash5@gmail.com>
2017-07-31 20:53:35 +0530

    loginsignup

--- server/server.js
+++ server/server.js
@@ -2,18 +2,139 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+// var mongoose = require('mongoose/');
+var {MongoClient}=require('mongodb');
 const {generateMessage, generateLocationMessage} = require('./message');
 const {isRealString} = require('./validation');
 const {Users}=require('./users');
+var session = require('express-session');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+app.use(session({
+    secret: 'node-chat-app',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(express.static(publicPath));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+  
+//var url = 'mongodb://akash:clash22@ds127443.mlab.com:27443/node-chat-app';      
+var url='mongodb://localhost:27017/Auth';
+//mongoose.connect(url);
+var _db;
+ MongoClient.connect(url, function (err, db) {
+  if (err) 
+   return console.log('Unable to connect to the mongoDB server. Error:', err);
+  else 
+    console.log('Connection established to', url);
+       _db=db;
+    });    
+app.post('/signup', function(req, res) {
+    var user = req.body.name;
+    var pass = req.body.pass;
+    var pass2 = req.body.pass2;
+
+    console.log(user,pass,pass2);
+   
+    
+    _db.collection('Users').insertOne({
+      username:user,
+      password:pass
+    }, (err,result)=>{
+      if(err)
+      return console.log('Unable to insert data',err);
+      console.log(JSON.stringify(result.ops,undefined,2));
+    });
+     // req.session.username=user; 
+    res.redirect(301, '/chat.html');
+res.end();
+});
+
+app.get('/login', function(req, res) {
+  res.sendFile(publicPath+'/login.html');
+});
+app.get('/signup', function(req, res) {
+  res.sendFile(publicPath+'/signup.html');
+});
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/loginSuccess',
+    failureRedirect: '/loginFailure',
+    session:true
+  })
+);
+
+app.get('/loginFailure', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
+
+app.get('/loginSuccess', function(req, res, next) {
+  
+   console.log('Logged in',req.user,req.user.username,req.user.password);
+   res.redirect('/room');
+});
+
+app.get('/room',function(req,res){
+  res.sendFile(publicPath+'/index2.html');
+});
+app.get('/api/user_data', function(req, res) {
+
+            if (req.user === undefined) {
+                // The user is not logged in
+                res.json({});
+            } else {
+                res.json({
+                    username: req.user.username
+                });
+            }
+        });
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  process.nextTick(function() {
+    _db.collection('Users').findOne({username:username}).then((docs)=>{
+        console.log(JSON.stringify(docs,undefined,2));
+         if (!docs) {
+        return done(null, false);
+      }
+
+      if (docs.password != password) {
+        return done(null, false);
+      }
+     
+      return done(null, docs);
+     },(err)=>{
+         console.log(err);
+         return done(err);
+     });
+  })
+}));
+
+  
 var server = http.createServer(app);
 var io = socketIO(server);
 var users=new Users();
-app.use(express.static(publicPath));
-
 io.on('connection', (socket) => {
   console.log('New user connected');
 
